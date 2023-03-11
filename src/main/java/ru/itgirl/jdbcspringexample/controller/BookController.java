f7547b87272c4f6435794d8bb3e58c2b9da1991f
aigulmal <imalg95@gmail.com>
2023-03-11 19:38:51 +0900

    17

---
+++ src/main/java/ru/itgirl/jdbcspringexample/controller/BookController.java
@@ -0,0 +1,28 @@
+package ru.itgirl.jdbcspringexample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itgirl.jdbcspringexample.model.Book;
+import ru.itgirl.jdbcspringexample.repository.BookRepository;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+    private static BookRepository bookRepository;
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/book/all")
+    public List<Book> getAllBooks() {
+        return bookRepository.findAllBooks();
+    }
+
+    @GetMapping("/book/{id}")
+    public static Book findBookById(@PathVariable("id") Long id) {
+       return bookRepository.findBookById(id);
+    }
+}
---
+++ src/main/java/ru/itgirl/jdbcspringexample/repository/BookRepositoryImpl.java
@@ -0,0 +1,65 @@
+package ru.itgirl.jdbcspringexample.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.itgirl.jdbcspringexample.model.Book;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public abstract class BookRepositoryImpl implements BookRepository {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public List<Book> findAllBooks() {
+
+        List<Book> result = new ArrayList<>();
+
+        String SQL_findAllBooks = "select * from books;";
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(SQL_findAllBooks)) {
+
+            while (resultSet.next()) {
+                Book book = convertRowToBook(resultSet);
+                result.add(book);
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        return result;
+    }
+
+    @Override
+    public Book findBookById(Long id) {
+        Book result = new Book();
+
+        try (Connection connection = dataSource.getConnection();
+            ) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM books where id =?");
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery(); {
+                while (resultSet.next()) {
+                    Book book = convertRowToBook(resultSet);
+                    result = book;
+                }
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        return result;
+    }
+
+
+    private Book convertRowToBook(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getLong("id");
+        String name = resultSet.getString("name");
+        return new Book(id, name);
+    }
+}
