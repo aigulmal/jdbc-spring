b7f2f7bfef48cfff7e980ef3072d8d0b2fd9b8ff
aigulmal <imalg95@gmail.com>
2023-03-11 19:59:59 +0900

    17_bookimpl

--- src/main/java/ru/itgirl/jdbcspringexample/repository/BookRepositoryImpl.java
+++ src/main/java/ru/itgirl/jdbcspringexample/repository/BookRepositoryImpl.java
@@ -14,6 +14,9 @@ import java.util.List;
 public abstract class BookRepositoryImpl implements BookRepository {
     @Autowired
     private DataSource dataSource;
+    public BookRepositoryImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     public List<Book> findAllBooks() {
